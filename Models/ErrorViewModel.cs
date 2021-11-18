7c74a01ed05fd0cac52012869584464dcda6955a
m3xpl4y <m3xpl4y@gmail.com>
2021-11-18 17:35:31 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerMySql.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
