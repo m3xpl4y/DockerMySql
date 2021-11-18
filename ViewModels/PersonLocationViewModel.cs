753e5da4fb871ea0c9427918aebf62857f722c98
m3xpl4y <m3xpl4y@gmail.com>
2021-11-18 18:03:30 +0100

    added properties to PersonLocationViewModel

--- ViewModels/PersonLocationViewModel.cs
+++ ViewModels/PersonLocationViewModel.cs
@@ -1,4 +1,17 @@
 ﻿namespace DockerMySql.ViewModels;
-    public class PersonLocationViewModel
-    {
-    }
+public class PersonLocationViewModel
+{
+    public int Id { get; set; }
+    [StringLength(60)]
+    [Display(Name = "Vorname")]
+    public string FirstName { get; set; }
+    [StringLength(60)]
+    [Display(Name = "Nachname")]
+    public string LastName { get; set; }
+    [StringLength(3)]
+    [Display(Name = "Alter")]
+    public int Age { get; set; }
+    [StringLength(100)]
+    [Display(Name = "Location")]
+    public string LocationName { get; set; }
+}
